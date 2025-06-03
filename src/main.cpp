e5dd1a4c6fe039e28e1f7dea75120622eac24955
Letsuchinoko <131267782+Letsuchinoko@users.noreply.github.com>
2025-06-03 17:35:22 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include "lvgl.h"
-
+lv_obj_t* temp;
+lv_obj_t* imperator;
+lv_obj_t* R;
+lv_obj_t* G;
+lv_obj_t* B;
+lv_obj_t* C;
+uint16_t r, g, b, c, colorTemp, lux;
 static void event_handler(lv_event_t * e)
 {
     lv_event_code_t code = lv_event_get_code(e);
@@ -17,26 +23,29 @@ void testLvgl()
   // Initialisations générales
   lv_obj_t * label;
 
+  temp=lv_label_create(lv_screen_active());
+  imperator=lv_label_create(lv_screen_active());
+  R=lv_label_create(lv_screen_active());
+  G=lv_label_create(lv_screen_active());
+  B=lv_label_create(lv_screen_active());
+  C=lv_label_create(lv_screen_active());
+  lv_obj_align(temp, LV_ALIGN_CENTER,0,-100);
+  lv_obj_align(imperator, LV_ALIGN_CENTER,0,-80);
+  lv_obj_align(R, LV_ALIGN_CENTER,0,-60);
+  lv_obj_align(G, LV_ALIGN_CENTER,0,-40);
+  lv_obj_align(B, LV_ALIGN_CENTER,0,-20);
+  lv_obj_align(C, LV_ALIGN_CENTER,0,0);
+  lv_label_set_text(temp,"");
+  lv_label_set_text(imperator, "");
   lv_obj_t * btn1 = lv_button_create(lv_screen_active());
   lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
-  lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
+  lv_obj_align(btn1, LV_ALIGN_CENTER, 0, 40);
   lv_obj_remove_flag(btn1, LV_OBJ_FLAG_PRESS_LOCK);
 
   label = lv_label_create(btn1);
   lv_label_set_text(label, "Bouton");
   lv_obj_center(label);
-
-  lv_obj_t * btn2 = lv_button_create(lv_screen_active());
-  lv_obj_add_event_cb(btn2, event_handler, LV_EVENT_ALL, NULL);
-  lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 40);
-  lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
-  lv_obj_set_height(btn2, LV_SIZE_CONTENT);
-
-  label = lv_label_create(btn2);
-  lv_label_set_text(label, "Toggle");
-  lv_obj_center(label);
 }
-
 #ifdef ARDUINO
 
 #include "lvglDrivers.h"
@@ -44,6 +53,7 @@ void testLvgl()
 #include "Adafruit_TCS34725.h"
 #define SCL 32;
 #define SDA 31;
+byte gammatable[256];
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
 // à décommenter pour tester la démo
 // #include "demos/lv_demos.h"
@@ -62,14 +72,13 @@ void mySetup()
 }
 
 void loop()
-{
-  uint16_t r, g, b, c, colorTemp, lux;
-
+{ 
   tcs.getRawData(&r, &g, &b, &c);
   // colorTemp = tcs.calculateColorTemperature(r, g, b);
   colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
   lux = tcs.calculateLux(r, g, b);
-
+  int temperature=colorTemp;
+  int lumiere=lux;
   Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
   Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
   Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
@@ -77,6 +86,8 @@ void loop()
   Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
   Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
   Serial.println(" ");
+  lv_label_set_text_fmt(temp, "%d", temperature);
+  lv_label_set_text_fmt(imperator,"%d", lumiere);
 }
 
 void myTask(void *pvParameters)

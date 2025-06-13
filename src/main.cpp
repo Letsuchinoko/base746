7c7a93196c16d4e3502e3a9bac360f7658b9e063
Letsuchinoko <131267782+Letsuchinoko@users.noreply.github.com>
2025-06-13 17:26:40 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include "lvgl.h"
-lv_obj_t* temp;
-lv_obj_t* imperator;
 lv_obj_t* R;
 lv_obj_t* G;
 lv_obj_t* B;
-lv_obj_t* C;
 lv_obj_t* Couleur;
-int temperature, lumiere, rouge, vert, bleu, contraste;
+lv_obj_t * sliderouge;
+lv_obj_t * slidervert;
+lv_obj_t * sliderbleu;
+int rouge, vert, bleu,sr,sv,sb;
 float red,green,blu;
 uint16_t r, g, b, c, colorTemp, lux;
+static void slider_event_cb_r(lv_event_t * e);
+static void slider_event_cb_v(lv_event_t * e);
+static void slider_event_cb_b(lv_event_t * e);
 static void event_handler(lv_event_t * e)
 {
     lv_event_code_t code = lv_event_get_code(e);
@@ -16,44 +19,43 @@ static void event_handler(lv_event_t * e)
     if(code == LV_EVENT_CLICKED) {
       LV_LOG_USER("Clicked");
     }
-    else if(code == LV_EVENT_VALUE_CHANGED) {
-        LV_LOG_USER("Toggled");
-    }
 }
 
 void testLvgl()
 {
   // Initialisations générales
   lv_obj_t * label;
-
-  temp=lv_label_create(lv_screen_active());
-  imperator=lv_label_create(lv_screen_active());
   R=lv_label_create(lv_screen_active());
   G=lv_label_create(lv_screen_active());
   B=lv_label_create(lv_screen_active());
-  C=lv_label_create(lv_screen_active());
   Couleur=lv_label_create(lv_screen_active());
-  lv_obj_align(temp, LV_ALIGN_CENTER,0,-100);
-  lv_obj_align(imperator, LV_ALIGN_CENTER,0,-80);
-  lv_obj_align(R, LV_ALIGN_CENTER,0,-60);
-  lv_obj_align(G, LV_ALIGN_CENTER,0,-40);
-  lv_obj_align(B, LV_ALIGN_CENTER,0,-20);
-  lv_obj_align(C, LV_ALIGN_CENTER,0,0);
-  lv_obj_align(Couleur, LV_ALIGN_CENTER,0,20);
-  lv_label_set_text(temp, "Temperature");
-  lv_label_set_text(imperator, "Luminosite");
-  lv_label_set_text(R, "Rouge");
-  lv_label_set_text(G, "Vert");
-  lv_label_set_text(B, "Bleu");
-  lv_label_set_text(C, "0");
+  sliderouge= lv_slider_create(lv_screen_active());
+  slidervert= lv_slider_create(lv_screen_active());
+  sliderbleu= lv_slider_create(lv_screen_active());
+  lv_obj_align(sliderouge, LV_ALIGN_CENTER,0,-90);
+  lv_obj_align(slidervert, LV_ALIGN_CENTER,0,-60);
+  lv_obj_align(sliderbleu,LV_ALIGN_CENTER,0,-30);
+  lv_obj_align(R, LV_ALIGN_CENTER,0,10);
+  lv_obj_align(G, LV_ALIGN_CENTER,0,30);
+  lv_obj_align(B, LV_ALIGN_CENTER,0,50);
+  lv_obj_align(Couleur, LV_ALIGN_CENTER,0,70);
+  lv_slider_set_range(sliderouge, 0, 255);
+  lv_slider_set_range(slidervert, 0, 255);
+  lv_slider_set_range(sliderbleu, 0, 255);
+  lv_label_set_text(R, "Rouge: 0");
+  lv_label_set_text(G, "Vert: 0");
+  lv_label_set_text(B, "Bleu: 0");
   lv_label_set_text(Couleur, "Couleur");
   lv_obj_t * btn1 = lv_button_create(lv_screen_active());
+  lv_obj_add_event_cb(sliderouge, slider_event_cb_r, LV_EVENT_VALUE_CHANGED, NULL);
+  lv_obj_add_event_cb(slidervert, slider_event_cb_v, LV_EVENT_VALUE_CHANGED, NULL);
+  lv_obj_add_event_cb(sliderbleu, slider_event_cb_b, LV_EVENT_VALUE_CHANGED, NULL);
   lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
-  lv_obj_align(btn1, LV_ALIGN_CENTER, 0, 60);
+  lv_obj_align(btn1, LV_ALIGN_CENTER, 0, 100);
   lv_obj_remove_flag(btn1, LV_OBJ_FLAG_PRESS_LOCK);
 
   label = lv_label_create(btn1);
-  lv_label_set_text(label, "Bouton");
+  lv_label_set_text(label, "Comparer couleur");
   lv_obj_center(label);
 }
 #ifdef ARDUINO
@@ -89,75 +91,65 @@ void loop()
   // colorTemp = tcs.calculateColorTemperature(r, g, b);
   colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
   lux = tcs.calculateLux(r, g, b);
-  temperature=colorTemp;
-  lumiere=lux;
-  contraste=c;
   tcs.getRGB(&red,&green,&blu);
   rouge=(unsigned int)red;
   vert=(unsigned int)green;
   bleu=(unsigned int)blu;
+  sr=(int)lv_slider_get_value(sliderouge);
+  sv=(int)lv_slider_get_value(slidervert);
+  sb=(int)lv_slider_get_value(sliderbleu);
   Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
   Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
-  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+  Serial.print("R: "); Serial.print(rouge, DEC); Serial.print(" ");
+  Serial.print("G: "); Serial.print(vert, DEC); Serial.print(" ");
+  Serial.print("B: "); Serial.print(bleu, DEC); Serial.print(" ");
   Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
   Serial.println(" ");
-  lv_label_set_text_fmt(temp, "Temperature :%dK", temperature);
-  lv_label_set_text_fmt(imperator,"Luminosite :%dlx", lumiere);
-  lv_label_set_text_fmt(R, "Rouge :%d",rouge);
-  lv_label_set_text_fmt(G, "Vert :%d",vert);
-  lv_label_set_text_fmt(B, "Bleu :%d",bleu);
-  lv_label_set_text_fmt(C, "%d",contraste);
   if (r>500&&g>500&&b>500)
   {
-    lv_label_set_text(Couleur,"Blanc");
     lv_obj_set_style_bg_color(scr,col=lv_color_make(255, 255, 255),LV_PART_MAIN);
     lv_obj_set_style_text_color(scr,col=lv_color_make(0, 0, 0),LV_PART_MAIN);
   }
   else if (r<100&&g<100&&b<100)
   {
-    lv_label_set_text(Couleur, "Noir");
     lv_obj_set_style_bg_color(scr,col=lv_color_make(0, 0, 0),LV_PART_MAIN);
     lv_obj_set_style_text_color(scr,col=lv_color_make(255, 255, 255),LV_PART_MAIN);
   }
   
   else if (g>300&r<300&b<400)
   {
-    lv_label_set_text(Couleur,"Vert");
     lv_obj_set_style_bg_color(scr,col=lv_color_make(0, 255, 0),LV_PART_MAIN);
     lv_obj_set_style_text_color(scr,col=lv_color_make(0, 0, 0),LV_PART_MAIN);
   }
   else if(b>300&r<300&g<300)
   {
-    lv_label_set_text(Couleur,"Bleu");
     lv_obj_set_style_bg_color(scr,col=lv_color_make(0, 0, 255),LV_PART_MAIN);
     lv_obj_set_style_text_color(scr,col=lv_color_make(0, 0, 0),LV_PART_MAIN);
   }
   else if(r>200&&g<200&b<200)
   {
-    lv_label_set_text(Couleur, "Rouge");
     lv_obj_set_style_bg_color(scr,col=lv_color_make(255, 0, 0),LV_PART_MAIN);
     lv_obj_set_style_text_color(scr,col=lv_color_make(0, 0, 0),LV_PART_MAIN);
   }
   else if(rouge>=108&&rouge<=120)
   {
-    lv_label_set_text(Couleur,"Orange");
     lv_obj_set_style_bg_color(scr,col=lv_color_make(255, 128, 0),LV_PART_MAIN);
     lv_obj_set_style_text_color(scr,col=lv_color_make(0, 0, 0),LV_PART_MAIN);
   }
   else if(rouge==75&&vert>=100&&vert<=104)
   {
-    lv_label_set_text(Couleur,"Jaune");
     lv_obj_set_style_bg_color(scr,col=lv_color_make(255, 255, 0),LV_PART_MAIN);
     lv_obj_set_style_text_color(scr,col=lv_color_make(0, 0, 0),LV_PART_MAIN);
   }
   else
   {
-    lv_label_set_text(Couleur,"Couleur non specifie");
-    lv_obj_set_style_bg_color(scr,col=lv_color_make(255, 255, 255),LV_PART_MAIN);
-    lv_obj_set_style_text_color(scr,col=lv_color_make(0, 0, 0),LV_PART_MAIN);
+    lv_obj_set_style_bg_color(scr,col=lv_color_make(147, 21, 50),LV_PART_MAIN);
+    lv_obj_set_style_text_color(scr,col=lv_color_make(255, 255, 255),LV_PART_MAIN);
   }
+  if(sr==rouge&&sv==vert&&sb==bleu)
+      {
+        lv_label_set_text(Couleur, "Meme couleur");
+      }
 }
 
 void myTask(void *pvParameters)
@@ -175,7 +167,27 @@ void myTask(void *pvParameters)
     vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(200)); // toutes les 200 ms
   }
 }
-
+static void slider_event_cb_r(lv_event_t * e)
+{
+    lv_obj_t * slider = lv_event_get_target_obj(e);
+    char buf[20];
+    lv_snprintf(buf, sizeof(buf), "Rouge: %d", (int)lv_slider_get_value(slider));
+    lv_label_set_text(R, buf);
+}
+static void slider_event_cb_v(lv_event_t * e)
+{
+    lv_obj_t * slider = lv_event_get_target_obj(e);
+    char buf[20];
+    lv_snprintf(buf, sizeof(buf), "Vert: %d", (int)lv_slider_get_value(slider));
+    lv_label_set_text(G, buf);
+}
+static void slider_event_cb_b(lv_event_t * e)
+{
+    lv_obj_t * slider = lv_event_get_target_obj(e);
+    char buf[20];
+    lv_snprintf(buf, sizeof(buf), "Bleu: %d", (int)lv_slider_get_value(slider));
+    lv_label_set_text(B, buf);
+}
 #else
 
 #include "lvgl.h"
